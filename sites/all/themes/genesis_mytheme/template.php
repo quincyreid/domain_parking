a6a7c67113359d59b82cc968d0766e423dfb6bd5
quincyreid <qreid51@gmail.com>
2012-12-08 13:11:41 -0500

    set site_name to SERVER_NAME

--- sites/all/themes/genesis_mytheme/template.php
+++ sites/all/themes/genesis_mytheme/template.php
@@ -14,11 +14,29 @@
  * Override or insert variables into all templates.
  */
 
-function genesis_mytheme_preprocess(&$vars, $hook) {
-  dpm($variables);
+ function genesis_mytheme_preprocess_page(&$vars) {
+   global $theme;
+   // Set variables for the logo and site_name.
+   if ($vars['logo']) {
+     $vars['site_logo'] = '<a href="' . $vars['front_page'] . '" title="' . t('Home page') . '" rel="home"><img src="' . $vars['logo'] . '" alt="' . $vars['site_name'] . ' ' . t('logo') . '" /></a>';
+   }
+   if ($vars['site_name']) {
+     $vars['site_name'] = $_SERVER['SERVER_NAME'];
+   }
+   // Set variables for the primary and secondary links.
+   $vars['main_menu_links'] = theme('links__system_main_menu', array('links' => $vars['main_menu'], 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'clearfix')), 'heading' => t('Main menu')));
+   $vars['secondary_menu_links'] = theme('links__system_secondary_menu', array('links' => $vars['secondary_menu'], 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'clearfix')), 'heading' => t('Secondary menu'))); 
+ }
+ 
+ 
+
+
+/*function genesis_mytheme_preprocess_page(&$vars) {
+  dpm($vars);
   $variables['site_name'] = 'site_name';
   $request_path = variable_get($_SERVER['site_name']);
 }
+*/
 
 /* -- Delete this line if you want to use these functions
 function genesis_mytheme_process(&$vars, $hook) {
